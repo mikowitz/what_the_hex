763818abe76f5254d6b111e9a71bf394da35f149
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-02-12 17:16:32 -0500

    Homepage

--- lib/what_the_hex_web/views/page_view.ex
+++ lib/what_the_hex_web/views/page_view.ex
@@ -1,7 +1,16 @@
 defmodule WhatTheHexWeb.PageView do
   use WhatTheHexWeb, :view
 
+  alias WhatTheHexWeb.Endpoint
+
   def render("show.json", %{data: data}) do
     data
+  end
+
+  def link_for_package(package) do
+    url = Endpoint.url() <> "/" <> package
+    [_, path] = String.split(url, "//")
+
+    {path, url}
   end
 end
